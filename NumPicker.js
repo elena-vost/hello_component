c5b015b9ccc22e60f48a5a17e430b10740cd64b0
elena-vost <angara1298@gmail.com>
2021-02-13 18:16:38 +0100

    standart react app layout

---
+++ NumPicker.js
@@ -0,0 +1,23 @@
+function getNum() {
+    return Math.floor(Math.random() * 10) +1;
+}
+class NumPicker extends React.Component {
+    render (){
+        const num = getNum();
+        let msg;
+        if(num === 7) {
+            msg = <div>
+                <h2>CONGRATS YOU WIN!</h2>
+                <img src ="https://i.pinimg.com/originals/8d/a3/cc/8da3cc2c3db6b45b56eb24b622c44c11.gif" />
+            </div>
+        } else {
+            msg = <p>SORRY! YOU LOSE</p>
+        }
+        return (
+            <div>
+                <h1>Your number is: {  num} </h1>
+                    {msg}
+            </div>
+        );
+    }
+}
