df8199bc6a0455190e7dbf79bc138fcd0518f2be
elena-vost <angara1298@gmail.com>
2021-02-14 08:13:38 +0100

    props beginning

--- Hello.js
+++ Hello.js
@@ -1,5 +1,5 @@
 class Hello extends React.Component {
     render() {
-        return <h1>Hello there!</h1>
+        return <h1>Hi {this.props.to} from {this.props.from}!</h1>
     }
 }
--- index.js
+++ index.js
@@ -2,7 +2,8 @@ class App extends React.Component {
     render () {
         return (
         <div>
-            <Hello />
+            <Hello to="Jorge" from="Lena"/>
+            <p>Let's play a game:)))</p>
             <NumPicker />
         </div>
         );
